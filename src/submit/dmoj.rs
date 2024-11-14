2d6169c6d78d1f5af4db6a85ce4c0df7ea17fcff
Egor Kulikov <egor@egork.net>
2024-11-14 16:39:12 +0100

    Fix dmoj comments

--- src/submit/dmoj.rs
+++ src/submit/dmoj.rs
@@ -7,14 +7,14 @@ pub(crate) fn submit(url: &str) {
     let problem_id = {
         match url_regex.captures(url) {
             None => {
-                failure("Unexpected URL for kattis problem");
+                failure("Unexpected URL for dmoj problem");
                 return;
             }
             Some(caps) => caps[1].to_string(),
         }
     };
-    if !check_available("kattis") {
-        failure("Please install kattis-cli from https://github.com/Kattis/kattis-cli");
+    if !check_available("dmoj-submit") {
+        failure("Please install dmoj-submit from https://github.com/nils-emmenegger/dmoj-submit");
         return;
     }
     Command::new("dmoj-submit").args(&["submit", "--problem", problem_id.as_str(), "--language", "rust", "main/src/main.rs"]).status().unwrap();
